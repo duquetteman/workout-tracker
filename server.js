1106edce9196b0d7d1a1327bafede637f823e240
Patrick Duquette <duquetteman@gmail.com>
2020-02-13 23:26:26 -0600

    Working on models index.js and workout.js.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-// Setting up Express App
 const app = express();
 const PORT = process.env.PORT || 8000;
 
@@ -12,7 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// db mongo
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
