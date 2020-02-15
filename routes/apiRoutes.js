10feffe4ecc8f4d0718d08c9850142a087efe411
Patrick Duquette <duquetteman@gmail.com>
2020-02-14 19:48:04 -0600

    Working on api Routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,9 @@
 const Workout = require("../models/workout.js")
 
 module.exports = function (app) {
+
+    app.get("/api/workouts", 
+
+    app.post("/api/workouts",
+
+    app.put("/api/workouts/:id",
